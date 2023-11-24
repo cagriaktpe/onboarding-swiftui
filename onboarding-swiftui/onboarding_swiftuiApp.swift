b81fb0006df3856fd096e5f8a0b65e082636de5e
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-11-24 14:31:17 +0300

    Initial Commit

---
+++ onboarding-swiftui/onboarding_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  onboarding_swiftuiApp.swift
+//  onboarding-swiftui
+//
+//  Created by Samet Çağrı Aktepe on 24.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct onboarding_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
